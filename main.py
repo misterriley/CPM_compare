354b4e374196e4e879ff0e37b386ec190ed055dd
Steve Riley <misterriley@gmail.com>
2022-08-09 17:33:38 -0400

    End of day commit

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+import multiprocessing
+
 import scipy.io as sio
 import scipy.stats as stats
 import os
 import numpy as np
 from mpire import WorkerPool
 from sklearn.linear_model import LinearRegression, Lasso
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score, KFold
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -12,6 +14,13 @@ import matplotlib.pyplot as plt
 # MAT_FILES_PATH = "G:/.shortcut-targets-by-id/1Y42MQjJzdev5CtNSh2pJh51BAqOrZiVX/IMAGEN/CPM_mat/"
 MAT_FILES_PATH = "G:/My Drive/CPM_test_data"
 COLLAPSE_VECTORS = True
+N_FOLDS = 5  # if None, leave-out-out CV is used with no randomization
+N_REPEATS = 100
+RANDOMIZE_DATA = True
+N_JOBS = 5
+RUN_COMPARISON = False
+N_ALPHAS = 201
+DESCS = ["negative", "positive", "all"]
 
 
 class CPMMasker:
@@ -39,6 +48,8 @@ class CPMMasker:
 
         pos_masked = x_[:, pos_mask]
         neg_masked = x_[:, neg_mask]
+        n_pos = pos_masked.shape[1]
+        n_neg = neg_masked.shape[1]
 
         if COLLAPSE_VECTORS:
             pos_masked = pos_masked.sum(axis=1).reshape(-1, 1)
@@ -46,57 +57,104 @@ class CPMMasker:
 
         if mask_type == "positive":
             ret = pos_masked
+            n_params = n_pos
         elif mask_type == "negative":
             ret = neg_masked
+            n_params = n_neg
         else:
             if COLLAPSE_VECTORS:
                 ret = pos_masked - neg_masked
             else:
-                ret = x_[:, [pos_mask[i] or neg_mask[i] for i in range(len(pos_mask))]]
+                ret = x_[:, [pos_mask[i] or neg_mask[i] for i in range(len(self.corrs))]]
+            n_params = n_pos + n_neg
 
-        return ret
+        return ret, n_params
 
 
-def run_lasso_comparison(x_, y_, cv=5, n_jobs=-1):
-
+def run_lasso_comparison(x_, y_, cv=5, n_jobs_=-1):
     x_normed = (x_ - np.mean(x_, axis=0)) / np.std(x_, axis=0)
 
     for i in np.logspace(-3.5, -1.5, num=101, base=10):
         model = Lasso(alpha=i)
-        scores = cross_val_score(model, x_normed, y_, cv=cv, n_jobs=n_jobs)
+        scores = cross_val_score(model, x_normed, y_, cv=cv, n_jobs=n_jobs_)
         print(f"alpha = {i:.4f}: {scores.mean():.4f}")
         model.fit(x_normed, y_)
         non_zero_betas = np.count_nonzero(model.coef_)
         print(f"non_zero_betas = {non_zero_betas}")
 
 
-def get_masker(x_, y_):
-    return CPMMasker(x_, y_)
+def get_masker(fold_index, shared_objects_):
+    kf_ = shared_objects_["kf"]
+    x_ = shared_objects_["x"]
+    y_ = shared_objects_["y"]
+
+    train_indices = kf_[fold_index][0]
+    x_train, y_train = x_[train_indices], y_[train_indices]
+    return CPMMasker(x_train, y_train)
 
 
-def run_one_cpm(shared_objects_, alpha, desc_):
-    fold_masker_arr_, fold_y_arr_, x_, y_, num_peeps_ = shared_objects_
-    y_pred = np.ndarray(shape=(len(y_),), dtype=np.float32)
-    for i in range(num_peeps_):
+def run_one_cpm(alpha, shared_objects_):
+    kf_ = shared_objects_["kf"]
+    fold_masker_arr_ = shared_objects_["fold_masker_arr"]
+    desc_ = shared_objects_["desc"]
+    y_ = shared_objects_["y"]
+    x_ = shared_objects_["x"]
+    y_pred = np.zeros(y_.shape)
+    total_params = 0
+    for i in range(len(kf_)):
+        train_indices = kf_[i][0]
+        test_indices = kf_[i][1]
+
         fold_masker = fold_masker_arr_[i].clone()
-        fold_x_masked = fold_masker.get_x(alpha, desc_)
-        fold_y = fold_y_arr_[i].reshape(-1, 1)
+        train_x_masked, n_params = fold_masker.get_x(alpha, desc_)
+        train_y = y_[train_indices]
         model = LinearRegression()
-        model.fit(fold_x_masked, fold_y.reshape(-1, 1))
-        pred_x = fold_masker.get_x(alpha, desc_, x_[i].reshape(1, -1))
-        y_pred[i] = model.predict(pred_x)[0]
+        model.fit(train_x_masked, train_y)
+        test_x, n_params = fold_masker.get_x(alpha, desc_, x_[test_indices])
+        y_pred[test_indices] = model.predict(test_x)
+        total_params += n_params
     rho = stats.spearmanr(y_, y_pred)[0]
-    print(f"alpha = {alpha:.10f}: desc = {desc_}: rho = {rho:.4f}")
-    return alpha, rho
+    avg_params = total_params / len(kf_)
+    # print(f"alpha = {alpha:.8f}: desc = {desc_}: rho = {rho:.4f}: n_params = {avg_params:.4f}")
+    return alpha, rho, avg_params
+
+
+def print_efficiency(insights_, n_jobs_):
+    print(f"threads: {n_jobs_}, efficiency: {insights_['working_ratio'] * n_jobs_:.2f}")
+
+
+def display_data(data_df_, log_x_, log_y_, folds_, repeats):
+    ax = sns.lineplot(x="alpha",
+                      y="rho",
+                      data=data_df_,
+                      label="rho",
+                      legend=False)
+    ax2 = ax.twinx()
+    sns.lineplot(x="alpha",
+                 y="n_params",
+                 data=data_df_,
+                 ax=ax2,
+                 label="n_params",
+                 legend=False,
+                 color="red")
+    ax.figure.legend()
+    plt.title(desc)
+
+    if log_x_:
+        ax.set_xscale("log")
+        ax2.set_xscale("log")
+    if log_y_:
+        ax.set_yscale("log")
+        ax2.set_yscale("log")
+
+    save_file_name = f"{desc}_{folds_}fold_{repeats}_repeats_{'_logx' if log_x_ else ''}{'_logy' if log_y_ else ''}.png"
+
+    plt.savefig(save_file_name)
+    plt.close()
 
 
 if __name__ == '__main__':
-
-    import logging
-    logging.captureWarnings(True)
-
     for file in os.listdir(MAT_FILES_PATH):
-
         if not file.endswith(".mat"):
             continue
 
@@ -112,33 +170,39 @@ if __name__ == '__main__':
         utix = np.triu_indices(num_nodes, k=1)
         x = x[utix[0], utix[1], :].transpose()
 
-        RUN_COMPARISON = False
         if RUN_COMPARISON:
             run_lasso_comparison(x, y, cv=len(y))
 
-        fold_x_arr = [np.concatenate((x[:i], x[i + 1:]), axis=0) for i in range(num_peeps)]
-        fold_y_arr = [np.concatenate((y[:i], y[i + 1:]), axis=0) for i in range(num_peeps)]
+        job_count = N_JOBS if N_JOBS is not None else multiprocessing.cpu_count()
+        folds = N_FOLDS if N_FOLDS is not None else num_peeps
+        alphas = np.logspace(0, -6, num=N_ALPHAS, base=10)
 
-        with WorkerPool(n_jobs=None) as pool:
-            fold_masker_arr = pool.map(get_masker, zip(fold_x_arr, fold_y_arr))
+        data_dfs = [pd.DataFrame(columns=["alpha", "rho", "n_params"]) for _ in range(len(DESCS))]
+        with WorkerPool(n_jobs=job_count, enable_insights=True) as pool:
+            for repeat_idx in range(N_REPEATS):
+                print(f"Repeat {repeat_idx+1}/{N_REPEATS}")
+                kf = KFold(n_splits=folds, shuffle=RANDOMIZE_DATA)
+                shared_objects = {"x": x, "y": y, "kf": [f for f in kf.split(x)]}
 
-        shared_objects = (fold_masker_arr, fold_y_arr, x, y, num_peeps)
-        with WorkerPool(n_jobs=8, shared_objects=shared_objects) as pool:
-            alphas = np.logspace(0, -20, num=1001, base=np.exp(1))
+                shared_objects["fold_masker_arr"] = pool.map(get_masker,
+                                                             zip(range(folds), [shared_objects for _ in range(folds)]),
+                                                             chunk_size=folds / job_count)
 
-            for desc in ["all", "positive", "negative"]:
-                cpm_results = pool.map(run_one_cpm, zip(alphas, [desc] * len(alphas)))
+                for desc_index in range(len(DESCS)):
+                    desc = DESCS[desc_index]
+                    print(f"{desc}")
+                    shared_objects["desc"] = desc
+                    cpm_results = pool.map(run_one_cpm,
+                                           zip(alphas, [shared_objects for i in alphas]),
+                                           chunk_size=int(len(alphas) / job_count))
 
-                data_df = pd.DataFrame(cpm_results, columns=["alpha", "rho"])
-                data_df["log alpha"] = np.log10(data_df["alpha"])
-                data_df = data_df[data_df["rho"] > 0]  # don't care about negative rho
+                    cpm_df = pd.DataFrame(cpm_results, columns=["alpha", "rho", "n_params"])
+                    data_dfs[desc_index] = pd.concat([data_dfs[desc_index], cpm_df])
 
-                sns.lineplot(x="alpha", y="rho", data=data_df, label=desc)
-                plt.savefig(f"{desc}_cv_plot.png")
-                plt.show(block=False)
-                plt.close()
+                print_efficiency(pool.get_insights(), job_count)
 
-                sns.lineplot(x="log alpha", y="rho", data=data_df, label=desc)
-                plt.savefig(f"{desc}_cv_plot_log.png")
-                plt.show(block=False)
-                plt.close()
+        for desc_index in range(len(DESCS)):
+            grouped_data = data_dfs[desc_index]
+            grouped_data = grouped_data[grouped_data["rho"] > 0].groupby(by=["alpha"])  # don't care about negative rho
+            for log_x, log_y in ((True, True), (True, False), (False, True), (False, False)):
+                display_data(grouped_data.mean(), log_x, log_y)
