41afaf071ad1e68ac3b54529802658ba46f50b0e
Steve Riley <misterriley@gmail.com>
2022-08-18 17:53:01 -0400

    End of day

--- pca_tests.py
+++ pca_tests.py
@@ -1,74 +1,15 @@
-from matplotlib import pyplot as plt
-from sklearn.decomposition import PCA
+import time
+
+import pandas as pd
+from scipy.stats import spearmanr
+from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import KFold
 
 import data_loader
 import numpy as np
-import seaborn as sns
 from mpire import WorkerPool
-import time
-
-
-class MuZero:
-    def __init__(self, n_components, n_features, n_classes, n_workers=1):
-        self.n_components = n_components
-        self.n_features = n_features
-        self.n_classes = n_classes
-        self.n_workers = n_workers
-        self.worker_pool = WorkerPool(n_workers)
-
-    def fit(self, X, y):
-        self.X = X
-        self.y = y
-        self.matrices = self.worker_pool.map(get_eig, [self.X for _ in range(self.n_components)])
-        self.matrices = np.array(self.matrices)
-        self.matrices = self.matrices[:, :, 1]
-        self.matrices = self.matrices.reshape(self.n_components, self.n_features, self.n_features)
-        self.matrices
-
-    def transform(self, X):
-        return self.worker_pool.map(self.transform_data, [X for _ in range(self.n_components)])
-
-    def transform_data(self, X):
-        return np.dot(X, self.matrices)
-
-    def fit_transform(self, X, y):
-        self.fit(X, y)
-        return self.transform(X)
-
-
-class AdamOptimizer:
-    def __init__(self, learning_rate=0.001, beta1=0.9, beta2=0.999, epsilon=1e-8):
-        self.learning_rate = learning_rate
-        self.beta1 = beta1
-        self.beta2 = beta2
-        self.epsilon = epsilon
-        self.m = None
-        self.v = None
-        self.t = 0
-
-    def initialize(self, proj_):
-        self.m = np.zeros(proj_.shape)
-        self.v = np.zeros(proj_.shape)
-
-    def update(self, grad, proj_):
-        self.m = self.beta1 * self.m + (1 - self.beta1) * grad
-        self.v = self.beta2 * self.v + (1 - self.beta2) * grad ** 2
-        m_hat = self.m / (1 - self.beta1 ** (self.t + 1))
-        v_hat = self.v / (1 - self.beta2 ** (self.t + 1))
-        proj_ -= self.learning_rate * m_hat / (np.sqrt(v_hat) + self.epsilon)
-        self.t += 1
-        return proj_
-
-
-class PCATests:
-
-    def __init__(self):
-        self.data_loader = data_loader.DataLoader(protocol_c=["IMAGEN"],
-                                                  file_c=["mats_mid_bsl.mat"],
-                                                  y_col_c=["kirby_c_estimated_k_all_trials_fu2"],
-                                                  clean_data=True)
-
+from multiprocessing import Lock
+import torch
 
 def fisher_r_to_z(r):
     return np.arctanh(r)
@@ -78,73 +19,100 @@ def fisher_z_to_r(z):
     return np.tanh(z)
 
 
-def get_eig(shared_objects, i):
-    x = shared_objects
-    # print("Getting eig {}".format(i))
-    return np.linalg.eig(x[:, :, i])
+def gradient_descent(y_, x_, ridge_c_):
+    proj_vector = 2 * torch.rand(size=(x_.shape[0], 1)) - 1
+    proj_vector /= torch.norm(proj_vector)
+    reg_vector = 2 * torch.rand(size=(2, 1)) - 1
+
+    proj_vector.requires_grad = True
+    reg_vector.requires_grad = True
+    optimizer = torch.optim.Adam([proj_vector, reg_vector], lr=0.000003)
+    x_ = torch.from_numpy(x_).float()
+    y_ = torch.from_numpy(y_).float()
+
+    now = time.time()
+
+    for i in range(100000):
+        optimizer.zero_grad()
+        proj_var = (proj_vector.T @ (proj_vector.T @ x_).squeeze()).squeeze()
+        output = reg_vector[1] * torch.log(proj_var) + reg_vector[0]
+        loss = torch.sum((output - y_) ** 2)
+        loss_with_ridge = loss + ridge_c_ * torch.sum(proj_vector ** 2) + ridge_c_ * torch.sum(reg_vector ** 2)
+        loss_with_ridge.backward()
+        optimizer.step()
+
+        if time.time() - now > 3:
+            print("loss: ", loss_with_ridge.item())
+            gn = proj_vector.grad.detach().data.norm() ** 2 + reg_vector.grad.detach().data.norm() ** 2
+            print("gnorm:", np.sqrt(gn.item()))
+            print("")
+            now = time.time()
+
+    return proj_vector.detach().numpy(), reg_vector.detach().numpy()
 
 
-def loss(proj_, y_, matrices):
-    return np.sum([(np.log(proj_.T @ matrices[:, :, j] @ proj_) - y_[j]) ** 2 for j in range(len(y_))])
+def predicted_output(proj_, x_, reg_vector_):
+    return reg_vector_[1] * np.log(np.matmul(np.matmul(proj_, x_), proj_)) + reg_vector_[0]
+
+def job(shared_objects, k_):
+    kf_split_, x_, y_, lock, ridge_c_ = shared_objects
+
+    train_, test_ = kf_split_[k_]
+    train_x_ = x_[:, :, train_]
+    test_x_ = x_[:, :, test_]
+    train_y_ = y_[train_]
+    test_y_ = y_[test_]
+
+    proj_, reg = gradient_descent(train_y_, train_x_, ridge_c_)
+
+    test_predictions_ = np.array([predicted_output(proj_, test_x_[:, :, t], reg) for t in range(len(test_))])
+    return test_predictions_, test_y_
 
 
-def one_grad(proj_, y_, matrix):
-    return 2 * (np.log(proj_.T @ matrix @ proj_) - y_) * 1 / (proj_.T @ matrix @ proj_) * 2 * (proj_.T @ matrix)
-
-
-# gradient of the loss function with respect to proj
-def gradient(proj_, y_, matrices):
-    grads = [one_grad(proj_, y_[j], matrices[:, :, j]) for j in range(len(y_))]
-    ret = np.sum(grads, axis=0)
-
-    return ret
-
-
-def gradient_descent(proj_, y_, matrices, tolerance=0.01, max_iter=1000):
-    adam_optimizer = AdamOptimizer()
-    adam_optimizer.initialize(proj_)
-
-    for step in range(max_iter):
-        grad = gradient(proj_, y_, matrices)
-        proj_ = adam_optimizer.update(grad, proj_)
-        if step % 100 == 0:
-            print("proj norm: {}".format(np.linalg.norm(proj_)))
-            print("loss: {}".format(loss(proj_, y_, matrices)))
-            print("grad norm: {}".format(np.linalg.norm(grad)))
-
-        if np.linalg.norm(grad) < tolerance * np.sqrt(y_.shape[0]):
-            break
-    return proj_
-
-
-INITIAL_PROJ_LENGTH = 1
-K_FOLDS = 10
+N_JOBS = 1  # multiprocessing.cpu_count() - 2
+K_FOLDS = N_JOBS * 4
 
 if __name__ == "__main__":
 
-    pca_tests = PCATests()
-    for data_set in pca_tests.data_loader.data_sets:
+    dl = data_loader.DataLoader(
+        protocol_c=[
+            # "IMAGEN",
+            # "sadie-marie",
+            "test_data"
+        ],
+        file_c=None,
+        # [
+        # "mats_sst_fu2.mat",
+        # "rest_estroop_acc_interf_2460_cpm_ready.mat",
+        # "enback_estroop_acc_interf_2460_cpm_ready.mat",
+        # "stp_all_clean2.mat"
+        # ],
+        y_col_c=None,
+        clean_data=True)
+
+    for data_set in dl.data_sets:
         x = data_set.x
         y = data_set.y
         x_as_r = fisher_z_to_r(x)
         for i in range(x.shape[0]):
-            x_as_r[i, i, :] = 1
+            x_as_r[i, i, :] = 1  # the diagonal would be ~.99999 otherwise
 
-        kf = KFold(n_splits=K_FOLDS, shuffle=True)
-        for k in range(K_FOLDS):
-            train, test = next(kf.split(x_as_r))
-            train_x = x_as_r[:, :, train]
-            test_x = x_as_r[:, :, test]
-            train_y = y[train]
-            test_y = y[test]
+        ridge_test_df = pd.DataFrame(columns=["ridge_c", "mse", "mse_std", "spearman_r", "spearman_p"])
 
-            initial_random_proj = 2 * np.random.rand(x.shape[0]) - 1
-            initial_random_proj /= (np.linalg.norm(initial_random_proj) / INITIAL_PROJ_LENGTH)
+        for ridge_c in [0, *np.logspace(-6, 0, num=7, base=10)]:
+            print("starting ridge_c {}".format(ridge_c))
 
-            proj = gradient_descent(initial_random_proj, train_y, train_x)
+            kf = KFold(n_splits=K_FOLDS, shuffle=True)
+            kf_split = list(kf.split(y))
 
-            print("Projection: {}".format(proj))
+            with WorkerPool(n_jobs=N_JOBS, shared_objects=(kf_split, x_as_r, y, Lock(), ridge_c)) as pool:
+                results = pool.map(job, range(K_FOLDS))
 
-            test_predictions = [np.log(proj.T @ x_as_r[:, :, t] @ proj) for t in test]
-            spearman_rho = np.corrcoef(test_predictions, test_y)[0, 1]
-            print("Spearman rho: {}".format(spearman_rho))
+            test_predictions = np.hstack(np.array([x for x, y in results], dtype=object)).astype(np.float32)
+            test_y = np.hstack(np.array([y for x, y in results], dtype=object)).astype(np.float32)
+            corr = spearmanr(test_predictions, test_y)
+            print("Correlation: {}".format(corr))
+
+            ridge_test_df.loc[len(ridge_test_df)] = [ridge_c, mean_squared_error(test_y, test_predictions),
+                                                     np.std(test_predictions - test_y), corr[0], corr[1]]
+        ridge_test_df.to_csv("{}_ridge_test_df.csv".format(data_set.descriptor))
